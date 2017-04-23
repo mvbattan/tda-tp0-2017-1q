4177800d42030b6bd241de4c453ea43e25eea2eb
Alejandro Levinas <levinasale@gmail.com>
2017-04-23 20:42:50 -0300

    Saco las variables de clase src y dst en Edge

--- fallas/main.py
+++ fallas/main.py
@@ -3,7 +3,7 @@ from common.graph import create_graph_from_file
 from fallas.TarjanAP import TarjanAP
 
 def main():
-    g = create_graph_from_file(os.getcwd() + "/fallas/entradas/g1.txt", is_directed = False)
+    g = create_graph_from_file(os.getcwd() + "/fallas/entradas/g4.txt", is_directed = False)
     print(g)
 
     AP = TarjanAP(g).getArticulationPoints()
