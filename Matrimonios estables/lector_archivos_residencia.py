a65a267ed9c41435caebf2e4e96fca7942a80a03
Alejandro Levinas <levinasale@gmail.com>
2017-04-24 01:48:47 -0300

    Corrijo formato

--- Matrimonios estables/lector_archivos_residencia.py
+++ Matrimonios estables/lector_archivos_residencia.py
@@ -12,6 +12,7 @@ def hospitalsAmount(f, n):
         f.readline()
     return int(f.readline())
 
+
 def tableCreator(finput, qlines):
     table = []
     for i in range(qlines):
@@ -21,40 +22,44 @@ def tableCreator(finput, qlines):
         table.append(s)
     return table
 
+
 def beginMatching(student, prefered_by_students, prefered_by_hospitals):
     for hospital in prefered_by_students[student - 1]:
 
-		taken_match=[couple for couple in tentative_engagements if hospital == couple[1]]
+        taken_match = [couple for couple in tentative_engagements if hospital == couple[1]]
 
-		if (len(taken_match)==0):
-			tentative_engagements.append([student, hospital])
-			free_students.remove(student)
-			break
-		else:
-			current_student=prefered_by_hospitals[hospital-1].index(taken_match[0][0])
-			potential_student=prefered_by_hospitals[hospital-1].index(student)
+        if (len(taken_match) == 0):
+            tentative_engagements.append([student, hospital])
+            free_students.remove(student)
+            break
+        else:
+            current_student = prefered_by_hospitals[hospital - 1].index(taken_match[0][0])
+            potential_student = prefered_by_hospitals[hospital - 1].index(student)
 
-			if(current_student < potential_student):
-				free_students.remove(student)
-				free_students.append(taken_match[0][0])
-				taken_match[0][0]=student
-				break
+            if (current_student < potential_student):
+                free_students.remove(student)
+                free_students.append(taken_match[0][0])
+                taken_match[0][0] = student
+                break
+
 
 def initFreeStudents(stu_list):
-	for student in range(len(stu_list)):
-		free_students.append(student+1)
+    for student in range(len(stu_list)):
+        free_students.append(student + 1)
 
-def stableMatching(prefered_by_students,prefered_by_hospitals):
-	while(free_students):
-		beginMatching(free_students[0],prefered_by_students,prefered_by_hospitals)
 
-file_name=input("Ingrese el nombre del archivo: ")
-with open(file_name + ".txt","r") as f:
-	n=int(f.readline())
-	PREFERED_BY_HOSPITALS=tableCreator(f,n)
-	m=int(f.readline())
-	PREFERED_BY_STUDENTS=tableCreator(f,m)
-	initFreeStudents(PREFERED_BY_STUDENTS)
-	stableMatching(PREFERED_BY_STUDENTS,PREFERED_BY_HOSPITALS)
-	print("tentative_engagements")
-	print(tentative_engagements)
+def stableMatching(prefered_by_students, prefered_by_hospitals):
+    while (free_students):
+        beginMatching(free_students[0], prefered_by_students, prefered_by_hospitals)
+
+
+file_name = input("Ingrese el nombre del archivo: ")
+with open(file_name + ".txt", "r") as f:
+    n = int(f.readline())
+    PREFERED_BY_HOSPITALS = tableCreator(f, n)
+    m = int(f.readline())
+    PREFERED_BY_STUDENTS = tableCreator(f, m)
+    initFreeStudents(PREFERED_BY_STUDENTS)
+    stableMatching(PREFERED_BY_STUDENTS, PREFERED_BY_HOSPITALS)
+    print("tentative_engagements")
+    print(tentative_engagements)
